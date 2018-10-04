cd1e6689a429a40a3b0561610c6d34170994ce45
jonas <jonas.grunau@whatsbroadcast.com>
2018-10-04 22:43:41 +0200

    working...

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on("connection", () => {
+    console.log("user connected");
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, () => {
+    console.log("listening on *:3000");
 });
